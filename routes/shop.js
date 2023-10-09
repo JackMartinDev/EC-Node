9bbb341df4aed293b632d7df735d5010feebce9a
Jack Martin <jackmartin.dev@gmail.com>
2023-10-09 15:42:51 +0900

    First commit

---
+++ routes/shop.js
@@ -0,0 +1,17 @@
+import express from "express"
+
+import shopController from '../controllers/shop.js'
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+export default router;
