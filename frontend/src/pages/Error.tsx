96f30fa31c2003d2c499fc2d71ea86149b737037
Jack Martin <jackmartin.dev@gmail.com>
2023-10-17 11:26:17 +0900

    Added navigation to error page

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -1,25 +1,27 @@
 import { Text, Title } from "@mantine/core";
 import { ErrorResponse, useRouteError } from "react-router-dom"
+import Navigation from "../components/Navigation";
 
 const ErrorPage = ():JSX.Element =>{
     const error = useRouteError() as ErrorResponse;
 
     const title = "An error has occured!";
-    let message = "Something went wrong!"; 
+    let message; 
     
     switch(error.status){
-    case 500:
-        message = JSON.parse(error.data).message;
-        break;
-    case 404:
-        message = "Could not find resource or page.";
-        break;
-    default:
-        message = "Something went wrong!" 
+        case 500:
+            message = JSON.parse(error.data).message;
+            break;
+        case 404:
+            message = "Could not find resource or page.";
+            break;
+        default:
+            message = "Something went wrong!" 
     }
 
     return(
         <>
+            <Navigation/>
             <Title>{title}</Title>
             <Text>{message}</Text>
         </>
